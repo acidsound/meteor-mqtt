b1d9524d07103acefe3269695544caa4da49bcce
spectrum <jhlee@appsoulute.com>
2015-02-13 01:48:22 +0900

    initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# meteor-mqtt
-MQTT for Meteor
---
+++ package.js
@@ -0,0 +1,20 @@
+Package.describe({
+  name: 'spectrum:mqtt',
+  version: '0.0.1',
+  // Brief, one-line summary of the package.
+  summary: 'MQTT library for Meteor',
+  // URL to the Git repository containing the source code for this package.
+  git: 'https://github.com/acidsound/meteor-mqtt',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Npm.depends({
+  "mqtt": "1.0.8"
+});
+
+Package.onUse(function(api) {
+  api.addFiles('mqtt.js', 'server');
+  api.export('mqtt');
+});
