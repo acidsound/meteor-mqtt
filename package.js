5630b50e3730c125ca5f0728ebd8c738a8ee9d49
root <root@ubuntu.(none)>
2015-02-13 02:15:46 +0900

    update version

--- package.js
+++ package.js
@@ -1,12 +1,8 @@
 Package.describe({
   name: 'spectrum:mqtt',
-  version: '0.0.1',
-  // Brief, one-line summary of the package.
+  version: '0.0.2',
   summary: 'MQTT library for Meteor',
-  // URL to the Git repository containing the source code for this package.
   git: 'https://github.com/acidsound/meteor-mqtt',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
 });
 
@@ -15,7 +11,7 @@ Npm.depends({
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('0.9.0');
+  api.versionsFrom('1.0');
   api.addFiles('mqtt.js', 'server');
   api.export('mqtt');
 });
